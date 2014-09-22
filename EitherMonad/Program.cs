fd84759710c32d147d1e15f61b1f98bc1ae6a809
Gordon Watts <gordon@gordonandpaula.net>
2014-09-22 03:05:03 +0200

    Some first experiments

---
+++ EitherMonad/Program.cs
@@ -0,0 +1,40 @@
+﻿using Monad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EitherMonad
+{
+    class Program
+    {
+        public static Either<int, string> Two()
+        {
+            return () => 2;
+        }
+
+        public static Either<int, string> Error()
+        {
+            return () => "Error!!";
+        }
+        
+        /// <summary>
+        /// Test out the Either monad
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var r =
+                from lhs in Two()
+                from rhs in Two()
+                select lhs + rhs;
+
+            Console.WriteLine("The r is {0}", r.IsRight());
+            if (r.IsRight())
+            {
+                Console.WriteLine("The value is {0}", r.Right());
+            }
+        }
+    }
+}
---
+++ EitherWithObjectMonand/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Monad;
+
+namespace EitherWithObjectMonand
+{
+    class Simple
+    {
+        public static int Count { get; private set; }
+        public int GetIt() {
+            Count += 1;
+            return 5;
+        }
+    }
+
+    class Program
+    {
+        public static Either<Simple, string> Two()
+        {
+            return () => new Simple();
+        }
+
+        static void Main(string[] args)
+        {
+            var r = from obj in Two()
+                    select obj.GetIt();
+
+            Console.WriteLine(r.IsRight());
+            if (r.IsRight())
+            {
+                Console.WriteLine(r.Right());
+            }
+            Console.WriteLine("It was called {0} times", Simple.Count);
+
+            // And a second time, with a more regular functional syntax??
+
+            var r1 = Two().Select(s => s.GetIt());
+        }
+    }
+}
